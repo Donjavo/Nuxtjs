e62426d1ec926596c084c690de246732d966c7ca
Donjavo <javiermartinex14@gmail.com>
2021-01-24 23:57:44 -0600

    inicio app

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <h1>About us</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+      doloremque tempore eos perspiciatis beatae laboriosam vero tempora,
+      sapiente corporis ullam repellat numquam aspernatur at quaerat, debitis
+      corrupti aperiam, voluptas eius!
+    </p>
+  </div>
+</template> 
+
+ <script>
+export default {
+  head() {
+    return {
+      title: "Posts | Home",
+      meta: [
+        {
+          hid: "dexcription",
+          name: "description",
+          content: "website about posts",
+        },
+      ],
+      htmlAttrs: {
+        lang: "en",
+      },
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Welcome to my first nuxt app</h1>
+</template>
+
+<script>
+export default {
+  head(){
+    return{
+      title:'Application Nuxt Posts | Home',
+      meta:[
+        {
+          hid:"dexcription",
+          name:"description",
+          content:"website about posts"
+
+        }
+      ],
+      htmlAttrs:{
+        lang:'en'
+      }
+    }
+  }
+}
+</script>
